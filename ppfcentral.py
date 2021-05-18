c799d80ab8e11dce635d35307401299f62bb4e10
Beatriz Simoes <49455451+bzimons@users.noreply.github.com>
2021-05-18 11:21:50 -0300

    Código central
    Esse código importa e executa todos as funções de extração dos sites, salvando e atualizando a base de dados

--- ppfcentral.py
+++ ppfcentral.py
@@ -10,8 +10,6 @@ from itertools import compress
 
 
 # Definindo o diretório para salvar os arquivos
-# C:\Users\bolin\Desktop\codigos\mcti-sefip-ppfcd2020\output
-# D:\Users\beatriz.simoes\Desktop\codigos\mcti-sefip-ppfcd2020\output
 os.chdir(r'C:\Users\bolin\Desktop\codigos\mcti-sefip-ppfcd2020\output')
 #-------------------------- Criação da pasta diária -------------------
 dia = datetime.today().strftime('%y%m%d') # yy/mm/dd
@@ -27,113 +25,31 @@ else:
 #---------------------------------------------------------------------------
 #  (aqui ficarão as importações dos arquivos py para scraping diário)
 
-# from belmontforum import *
-from ICGEB import *
-from royalsociety import *
-from wellcome import *
-from tinker import *
-from arcadia import *
-from nationalgeographic import *
-from simons import *
-from cepf import *
-from foundationfar import *
-from itto import *
-from roddenberry import *
-from moore import *
-from pbnf import *
-from fapesp import *
-from fundoamazonia import *
-from biocodex import *
-from fonplata import *
-from impact import *
-from iadb import *
-from thegef import *
-from erefdn import *
-from funbio import *
-from irdc import *
-from nfwf import *
-from forestgeo import *
-from oceaninnovation import *
-from tinybeam import *
-from globalwomennet import *
-from twas import *
-from conservegrassland import *
-from euraxess import *
-from rufford import *
-
+from wellcome import * #Importa todas as funções
 
 keywords='(low- or middle- income country|brazil|latin american region|member countries of fonplata|developing countries)'
 
+# Executa as funções do código dos sites:
 try:
-  royal1('.\\'+dia,keywords) #função que importa
   wellcome1('.\\'+dia,keywords)
-  icgeb1('.\\'+dia,keywords)
-  # belmont1('.\\'+dia,keywords)
-  tinker1('.\\'+dia,keywords)
-  nationalgeographic1('.\\'+dia,keywords)
-  simonsfoundation1('.\\'+dia,keywords)
-  cepf1('.\\'+dia,keywords)
-  foundationfar1('.\\'+dia,keywords)
-  roddenberry1('.\\'+dia,keywords)
-  moore1('.\\'+dia,keywords)
-  pbnf1('.\\'+dia,keywords)
-  fapesp1('.\\'+dia,keywords)
-  itto1('.\\'+dia,keywords)
-  fonplata1('.\\'+dia,keywords)
-  impact1('.\\'+dia,keywords)
-  irdc1('.\\'+dia,keywords)
-  nfwf1('.\\'+dia,keywords)
-  forestgeo1('.\\'+dia,keywords)
-  oceaninnovationchallenge1('.\\'+dia,keywords)
-  tinybeamfund1('.\\'+dia,keywords)
-  globalwomennet1('.\\'+dia,keywords)
-  twas1('.\\'+dia,keywords)
-  conservegrassland1('.\\'+dia,keywords)
-  euraxess1('.\\'+dia,keywords)
-  rufford1('.\\'+dia,keywords)
-  # royal2('.\\'+dia)
-  # simonsfoundation2('.\\'+dia)
-  # fundoamazonia2('.\\'+dia)
-  # iadb2('.\\'+dia)
-  # arcadia2('.\\'+dia)
-  # foundationfar2('.\\'+dia)
-  # roddenberry2('.\\'+dia)
-  # royal3('.\\'+dia)
-  # wellcome2('.\\'+dia)
-  # wellcome3('.\\'+dia)    
-  # icgeb2('.\\'+dia)
-  # icgeb3('.\\'+dia)    
-  # thegef2('.\\'+dia)
-  # thegef3('.\\'+dia)
-  # erefdn3('.\\'+dia)
-  # arcadia4('.\\'+dia,keywords)
-  # fundoamazonia4('.\\'+dia,keywords)
-  # iadb4('.\\'+dia,keywords)
-  # thegef4('.\\'+dia)
-  # tinker4('.\\'+dia)
-  # erefdn4('.\\'+dia)
-  # funbio4('.\\'+dia)
-  # biocodexmicrobiotafundation4('.\\'+dia)
+  wellcome2('.\\'+dia)
+  wellcome3('.\\'+dia)    
+
 except Exception as e:
   pass
   print('Erro na função')
   print(e) 
   
-  
 
+# Funções
 
-#---------------------------------------------------------------------------
-#--------------------------------- Funções  -----------------------------
-#---------------------------------------------------------------------------
 #--------------- função que define o caminho da pasta------------------
 def paths(pasta,arquivo):
   path = '''.\\'''+pasta+'''\\'''+arquivo
   return(path)
-#---------------------------------------------------------------------
 
-# --------------------------- função que atualiza a base --------------
 
-# -------------------------------------------------------------------
+# --------------- função que atualiza a base --------------
 def atualizador(baseprincipal,diamaisrecente):
   diario = pd.read_csv(diamaisrecente)
   main = pd.read_csv(baseprincipal)
@@ -141,12 +57,6 @@ def atualizador(baseprincipal,diamaisrecente):
   x = re.search('03.csv$', baseprincipal) # Se for campo de política, o que é comparado é texto.
   if(x!=None):
     a = diario['pol_texto'].isin(main['pol_texto'])   # usar o tag/id criado inves do link
-    b = [not b for b in a] # inverter para o TRUE ser a linha que não tem no main
-    novaslinhas = diario[b]
-    main = main.append(novaslinhas,ignore_index=True) #novo main
-    main.to_csv(baseprincipal,index=False,sep=",")
-  elif(diario=="conservegrassland_01.csv"): # Esse site possui os links iguais
-    a = diario['opo_texto'].isin(main['opo_texto'])   # usar o tag/id criado inves do link
     b = [not b for b in a] # inverter para o TRUE ser a linha que não tem no main
     novaslinhas = diario[b]
     main = main.append(novaslinhas,ignore_index=True) #novo main
@@ -191,8 +101,6 @@ for i in range(0,len(filenames)):
         x = re.search('03.csv$', dia1) # Se for campo de política, o que é comparado é texto.
         if(x!=None):
           comp = pd.read_csv(dia1)['pol_texto'].isin(pd.read_csv(dia2)['pol_texto']) #Lista boleana comparando os textos das politicas
-        elif(dia1=="conservegrassland_01.csv"):
-           comp = pd.read_csv(dia1)['opo_texto'].isin(pd.read_csv(dia2)['opo_texto'])
         else:
           comp = pd.read_csv(dia1)['link'].isin(pd.read_csv(dia2)['link']) #Lista boleana comparando os links
         if all(comp)==False: # Verifica se um dos itens da lista de comparação de links é falsa para atualizar
